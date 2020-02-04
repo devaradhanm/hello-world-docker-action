9d0b7d9ca8c5dd1527040dd428fcf47169741040
devaradhanm <demuruge@microsoft.com>
2020-02-04 15:32:07 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@
 echo "Hello $1"
 time=$(date)
 
-node ./index.js
+echo "Starting http server..."
+http-server /workspace/_site -p 9123 &
 
+echo "Starting Scan..."
+ai-scan --url http://127.0.0.1:9123 --output /workspace/_report
+
+echo "Scan completed!"
 echo ::set-output name=time::$time
