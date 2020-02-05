ec842cb8e89e5e27e3b92e0f577a52c6ff08a13f
devaradhanm <demuruge@microsoft.com>
2020-02-04 17:13:45 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,15 @@
 #!/bin/sh -l
 
-echo "Hello $1"
 time=$(date)
 
-echo "files under /github/workspace"
-ls /github/workspace
-
-echo "files under /workspace/_site"
-ls /workspace/_site
+echo "files under /github/workspace/$1"
+ls /github/workspace/$1
 
 echo "Starting http server..."
-http-server /workspace/_site -p 9123 &
+http-server /github/workspace/$1 -p 9123 &
 
 echo "Starting Scan..."
-ai-scan --url http://127.0.0.1:9123 --output /workspace/_report
+ai-scan --url http://127.0.0.1:9123 --output /github/workspace/$2
 
 echo "Scan completed!"
 echo ::set-output name=time::$time
