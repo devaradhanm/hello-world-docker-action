5afa8abe16fca42f7ccd0d55817df5e1a866610c
devaradhanm <demuruge@microsoft.com>
2020-02-05 20:29:23 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,12 @@ echo "files under /github/workspace/$1"
 ls /github/workspace/$1
 
 
-echo "Starting http server..."
-npx http-server /github/workspace/$1 -p 9123 &
+#echo "Starting http server..."
+#npx http-server /github/workspace/$1 -p 9123 &
 
 echo "Starting Scan..."
-node /node_modules/accessibility-insights-scan/dist/ai-scan.js --url http://127.0.0.1:9123 --output /github/workspace/$2
+#node /node_modules/accessibility-insights-scan/dist/ai-scan.js --url http://127.0.0.1:9123 --output /github/workspace/$2
+node /node_modules/accessibility-insights-scan/dist/ai-scan.js --url https://www.bing.com --output /github/workspace/$2
 
 echo "Scan completed!"
 echo ::set-output name=time::$time
