8e867b1c1f3bcbd546d10310bff2a8740d1a9df9
devaradhanm <demuruge@microsoft.com>
2020-02-06 10:48:17 -0800

    Create run-scan.sh

---
+++ run-scan.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+
+ time=$(date)
+ #ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+
+ echo "who am i?"
+ whoami
+
+ echo "files under /github/workspace/$1"
+ ls /github/workspace/$1
+
+ #echo "Starting http server..."
+ #npx http-server /github/workspace/$1 -p 9123 &
+
+ echo "Starting Scan for bing..."
+ #node /node_modules/accessibility-insights-scan/dist/ai-scan.js --url http://127.0.0.1:9123 --output /github/workspace/$2
+ node /node_modules/accessibility-insights-scan/dist/ai-scan.js --url https://www.bing.com --output /github/workspace/$2
