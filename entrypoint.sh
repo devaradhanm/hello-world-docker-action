fd845829e5feb24c533fcdbe30812b178c218984
devaradhanm <demuruge@microsoft.com>
2020-02-05 23:20:22 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,29 @@
 #!/bin/sh -l
 
-echo "granting permissions to pptuser"
 chown -R pptruser:pptruser /github
-echo "changing user..."
-su -l pptruser
+exec runuser -u pptruser "$@"
 
-time=$(date)
-#ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+# echo "granting permissions to pptuser"
+# chown -R pptruser:pptruser /github
+# echo "changing user..."
+# su -l pptruser
 
-echo "who am i?"
-whoami
+# time=$(date)
+# #ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
 
-echo "files under /github/workspace/$1"
-ls /github/workspace/$1
+# echo "who am i?"
+# whoami
+
+# echo "files under /github/workspace/$1"
+# ls /github/workspace/$1
 
 
-#echo "Starting http server..."
-#npx http-server /github/workspace/$1 -p 9123 &
+# #echo "Starting http server..."
+# #npx http-server /github/workspace/$1 -p 9123 &
 
-echo "Starting Scan for bing..."
-#node /node_modules/accessibility-insights-scan/dist/ai-scan.js --url http://127.0.0.1:9123 --output /github/workspace/$2
-node /node_modules/accessibility-insights-scan/dist/ai-scan.js --url https://www.bing.com --output /github/workspace/$2
+# echo "Starting Scan for bing..."
+# #node /node_modules/accessibility-insights-scan/dist/ai-scan.js --url http://127.0.0.1:9123 --output /github/workspace/$2
+# node /node_modules/accessibility-insights-scan/dist/ai-scan.js --url https://www.bing.com --output /github/workspace/$2
 
-echo "Scan completed!"
-echo ::set-output name=time::$time
+# echo "Scan completed!"
+# echo ::set-output name=time::$time
